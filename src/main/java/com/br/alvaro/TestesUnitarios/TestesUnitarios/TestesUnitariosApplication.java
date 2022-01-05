d61d773888b5360f2157945c38fac77d0d5bb5db
Alvaro Pereira do Nascimento <alvaropereira15@gmail.com>
2022-01-04 21:56:12 -0300

    Criado processo com testes unitários.
    Co-Authored-By: Alvaro Pereira Nascimento <39337285+AlvaroNascimento1993@users.noreply.github.com>

--- src/main/java/com/br/alvaro/TestesUnitarios/TestesUnitarios/TestesUnitariosApplication.java
+++ src/main/java/com/br/alvaro/TestesUnitarios/TestesUnitarios/TestesUnitariosApplication.java
@@ -4,6 +4,8 @@ import com.br.alvaro.TestesUnitarios.TestesUnitarios.Service.LocacaoService;
 import com.br.alvaro.TestesUnitarios.TestesUnitarios.entidades.*;
 import com.br.alvaro.TestesUnitarios.TestesUnitarios.utils.DataUtils;
 import java.util.Date;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,20 +18,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestesUnitariosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestesUnitariosApplication.class, args);
+		public static void main(String[] args) {
+			SpringApplication.run(TestesUnitariosApplication.class, args);
+			
+			//cenário.
+				Usuario usuario = new Usuario("Alvaro Pereira do Nascimento");
+				Filme filme = new Filme("A bela e a Fera", 100, 10.00);
+				LocacaoService locacaoService = new LocacaoService();
+				//ação.
+				Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+				//verificação.
+				System.out.println(locacao.toString());
+				System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+				System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+				System.out.println(locacao.getValor() == 11.0);
+		}
 		
-		//cenário.
-		Usuario usuario = new Usuario("Alvaro Pereira do Nascimento");
-		Filme filme = new Filme("A bela e a Fera", 100, 10.00);
-		LocacaoService locacaoService = new LocacaoService();
-		//ação.
-		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
-		//verificação.
-		System.out.println(locacao.toString());
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		System.out.println(locacao.getValor() == 10.0);
-	}
-
+		@Test
+		public void teste(){
+			//cenário.
+			Usuario usuario = new Usuario("Alvaro Pereira do Nascimento");
+			Filme filme = new Filme("A bela e a Fera", 100, 10.00);
+			LocacaoService locacaoService = new LocacaoService();
+			//ação.
+			Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+			//verificação.
+			System.out.println(locacao.toString());
+			Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+			Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+			Assert.assertTrue(locacao.getValor() == 10.0);
+		}
 }
